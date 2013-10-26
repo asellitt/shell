c02594b122e0d49e9a9cf2945135ad84b221cfdd
Anthony Sellitti <anthony.sellitti@envato.com>
2013-10-26 20:51:20 +1100

    added more settings
    - osx defaults
    - alfred
    - sublime

--- install.sh
+++ install.sh
@@ -62,29 +62,92 @@ if [[ -z "$1" ]]; then
 
 
 
-    # slate
-    echo 'Installing Slate config'
-    rm ~/.slate 2>/dev/null
-    ln -s $DIR/slate/slate ~/.slate
+    # OSX specific stuffs
+    if [ `uname` == 'Darwin' ]; then
+        echo 'Installing OSX Specific config'
 
 
 
-    # sublime
-    echo 'Installing Sublime config'
-    SUBLDIR="$HOME/Library/Application Support/Sublime Text 2/Packages"
-    rm "$SUBLDIR"/User/*-keymap   2>/dev/null
-    ln -s $DIR/sublime/*-keymap   "$SUBLDIR"/User/
-    rm "$SUBLDIR"/User/*-settings 2>/dev/null
-    ln -s $DIR/sublime/*-settings "$SUBLDIR"/User/
-    rm "$SUBLDIR"/User/*-macro    2>/dev/null
-    ln -s $DIR/sublime/*-macro    "$SUBLDIR"/User/
-    rm "$SUBLDIR"/User/*-snippet  2>/dev/null
-    ln -s $DIR/sublime/*-snippet  "$SUBLDIR"/User/
-    rm "$SUBLDIR"/Color\ Scheme\ -\ Default/Blackbolt.tmTheme* 2>/dev/null
-    ln -s $DIR/sublime/theme "$SUBLDIR"/Color\ Scheme\ -\ Default/Blackbolt.tmTheme
-    rm "$SUBLDIR"/User/syntax_highlighting.py* 2>/dev/null
-    ln -s $DIR/sublime/syntax_highlighting.py "$SUBLDIR"/User/syntax_highlighting.py
+        # preferences
+        echo '  Setting OSX preferences'
+        chflags nohidden ~/Library
+        defaults write NSGlobalDomain            ApplePressAndHoldEnabled                       -bool FALSE
+        defaults write NSGlobalDomain            AppleKeyboardUIMode                            -int  3
+        defaults write NSGlobalDomain            PMPrintingExpandedStateForPrint                -bool TRUE
+        defaults write NSGlobalDomain            NSNavPanelExpandedStateForSaveMode             -bool TRUE
+        defaults write NSGlobalDomain            InitialKeyRepeat                               -int  35
+        defaults write NSGlobalDomain            KeyRepeat                                      -int  0
+        defaults write NSGlobalDomain            com.apple.swipescrolldirection                 -bool FALSE
+        defaults write NSGlobalDomain            com.apple.keyboard.fnState                     -bool TRUE
+        defaults write com.apple.dock            no-glass                                       -bool TRUE
+        defaults write com.apple.dock            autohide                                       -bool TRUE
+        defaults write com.apple.dock            checked-for-launchpad                          -bool TRUE
+        defaults write com.apple.dock            persistent-apps                                '()'
+        defaults write com.apple.dock            show-process-indicators                        -bool FALSE
+        defaults write com.apple.dock            tilesize                                       -int  36
+        defaults write com.apple.finder          ShowExternalHardDrivesOnDesktop                -bool TRUE
+        defaults write com.apple.finder          ShowHardDrivesOnDesktop                        -bool TRUE
+        defaults write com.apple.finder          ShowMountedServersOnDesktop                    -bool TRUE
+        defaults write com.apple.finder          ShowRemovableMediaOnDesktop                    -bool TRUE
+        defaults write com.apple.finder          EmptyTrashSecurely                             -bool TRUE
+        defaults write com.apple.universalaccess HIDScrollZoomModifierMask                      -int  262144
+        defaults write com.apple.LaunchServices  LSQuarantine                                   -bool FALSE
+        defaults write com.apple.desktopservices DSDontWriteNetworkStores                       -bool TRUE
+        defaults write com.apple.dashboard       mcx-disabled                                   -bool TRUE
+        defaults -currentHost write com.apple.screensaver     moduleDict                        -dict moduleName 'Flurry'
+        defaults -currentHost write com.apple.screensaver     moduleDict                        -dict path '/System/Library/Screen Savers/Flurry.saver'
+        defaults -currentHost write com.apple.screensaver     showClock                         -bool TRUE
 
+
+
+        PREFDIR="$HOME/Library/Preferences"
+        rm "$PREFDIR"/com.apple.symbolichotkeys.plist 2>/dev/null
+        ln -s $DIR/osx/com.apple.symbolichotkeys.plist "$PREFDIR"/
+
+
+
+        echo '  Restarting Apps'
+        killall Dock
+        killall Finder
+
+
+
+        # slate
+        echo '  Installing Slate config'
+        rm ~/.slate 2>/dev/null
+        ln -s $DIR/slate/slate ~/.slate
+
+
+
+        # sublime
+        echo '  Installing Sublime config'
+        SUBLDIR="$HOME/Library/Application Support/Sublime Text 2/Packages"
+        rm "$SUBLDIR"/User/*-keymap   2>/dev/null
+        ln -s $DIR/sublime/*-keymap   "$SUBLDIR"/User/
+        rm "$SUBLDIR"/User/*-settings 2>/dev/null
+        ln -s $DIR/sublime/*-settings "$SUBLDIR"/User/
+        rm "$SUBLDIR"/User/*-macro    2>/dev/null
+        ln -s $DIR/sublime/*-macro    "$SUBLDIR"/User/
+        rm "$SUBLDIR"/User/*-snippet  2>/dev/null
+        ln -s $DIR/sublime/*-snippet  "$SUBLDIR"/User/
+        rm "$SUBLDIR"/Color\ Scheme\ -\ Default/Blackbolt.tmTheme* 2>/dev/null
+        ln -s $DIR/sublime/theme "$SUBLDIR"/Color\ Scheme\ -\ Default/Blackbolt.tmTheme
+        rm "$SUBLDIR"/User/syntax_highlighting.py* 2>/dev/null
+        ln -s $DIR/sublime/syntax_highlighting.py "$SUBLDIR"/User/syntax_highlighting.py
+        SUBLDIR="$HOME/Library/Application Support/Sublime Text 2/Installed Packages"
+        rm "$SUBLDIR"/*-package   2>/dev/null
+        ln -s $DIR/sublime/*-package   "$SUBLDIR"/
+
+
+
+        # alfred
+        echo '  Installing Alfred config'
+        rm "$PREFDIR"/com.runningwithcrayons.Alfred-* 2>/dev/null
+        ln -s $DIR/alfred/*.plist "$PREFDIR"/
+        ALFDIR="$HOME/Library/Application Support/Alfred 2/Alfred.alfredpreferences"
+        rm -rf "$ALFDIR"/preferences #2>/dev/null
+        ln -s $DIR/alfred/preferences "$ALFDIR"
+    fi
 
 
 #
