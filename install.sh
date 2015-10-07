824b06c8208f8ac7de98a7d246b2f099824b7b9c
Anthony Sellitti <anthony.sellitti@envato.com>
2015-10-07 20:43:37 +1100

    commandline args, license acceptance, and other craps

--- install.sh
+++ install.sh
@@ -1,52 +1,78 @@
 #! /bin/bash
-
+PREFIX="INSTALL"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR}/install/functions.sh"
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -u|--update)
+      UPDATE=true
+      ;;
+    -l|--license)
+      LICENSE=true
+      ;;
+    -h|--help)
+      usage
+      exit
+      ;;
+    *)
+      echo "ERROR: unknown parameter: ${1}"
+      usage
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 
 #
 # default action: install environment
 #
 if [[ -z "$1" ]]; then
-    PREFIX="INSTALL"
+  log "Linking dotfiles"
 
-    ensure_lastpass_installed
-    . $DIR/install/modules.sh
+  ensure_secret_dir_exists
+  agree_to_xcode_license
+  ensure_lastpass_installed
+
+  . $DIR/install/modules.sh
+
 
 #
 # header: install a bash header
 #
 elif [[ "$1" == "header" ]]; then
-    HEADDIR="$DIR/bash/header"
-    if [[ -z "$2" ]]; then
-        echo 'Install which header?'
+  HEADDIR="$DIR/bash/header"
+  if [[ -z "$2" ]]; then
+    echo 'Install which header?'
+  else
+    if [[ -f "${HEADDIR}/${2}" ]]; then
+      echo "Installing header: ${HEADDIR}/${2}"
+      rm ~/.bash_header 2>/dev/null
+      ln -s $HEADDIR/$2 ~/.bash_header
     else
-        if [[ -f "${HEADDIR}/${2}" ]]; then
-            echo "Installing header: ${HEADDIR}/${2}"
-            rm ~/.bash_header 2>/dev/null
-            ln -s $HEADDIR/$2 ~/.bash_header
-        else
-            echo "${HEADDIR}/${2} doesnt exist..."
-        fi
+      echo "${HEADDIR}/${2} doesnt exist..."
     fi
+  fi
 
 
 #
 # non-boxen: install binaries on a non-boxenated system
 #
 elif [[ "$1" == "nonboxen" ]]; then
-    if hash brew 2>/dev/null; then
-        echo 'Brew installed'
-        echo '  Installing gh...'
-        brew install gh
-        echo '  Installing autojump...'
-        brew install autojump
-        echo '  Installing the_silver_searcher...'
-        brew install the_silver_searcher
-    else
-        echo 'Install brew first: http://brew.sh'
-    fi
+  if hash brew 2>/dev/null; then
+    echo 'Brew installed'
+    echo '  Installing gh...'
+    brew install gh
+    echo '  Installing autojump...'
+    brew install autojump
+    echo '  Installing the_silver_searcher...'
+    brew install the_silver_searcher
+  else
+    echo 'Install brew first: http://brew.sh'
+  fi
 
 
 else
-    echo "Dont know what ${1} means"
+  echo "Dont know what ${1} means"
 fi
--- install/functions.sh
+++ install/functions.sh
@@ -16,6 +16,30 @@ function link() {
   ln -s "${FROM}" "${TO}"
 }
 
+function usage() {
+  echo "asellitt's dotfiles install script"
+  echo ""
+  echo "usage: install [-u|--update]"
+  echo "               [-l|--license]"
+  echo "               [-h|--help]"
+  echo ""
+}
+
+function ensure_secret_dir_exists() {
+  log "Ensuring secrets directory exist"
+  mkdir -p "${DIR}/secret"
+}
+
+function agree_to_xcode_license() {
+  if [[ $LICENSE == true ]]; then
+    if [ `uname` == 'Darwin' ]; then
+      log "OSX detected"
+      log "Agreeing to xcode license, because fuck you OSX"
+      sudo xcodebuild -license
+    fi
+  fi
+}
+
 function ensure_lastpass_installed() {
   if hash lpass 2>/dev/null; then
     log_into_lastpass
@@ -25,26 +49,38 @@ function ensure_lastpass_installed() {
 }
 
 function log_into_lastpass() {
-  log "lastpass-cli installed, username:"
-  read username
-  lpass login $username
+  if [[ -f "${HOME}/.lpass/username" ]]; then
+    log "lastpass-cli already logged in, continuing"
+  else
+    log "lastpass-cli installed, username:"
+    read username
+    lpass login $username
+  fi
 }
 
 function install_lastpass() {
   # lastpass-cli is a dependency for secrets, so ensure its available
   if hash boxen 2>/dev/null; then
-      log "Boxen detected, ensuring up to date"
-      boxen
-  else
-      log "Non-boxened machine detected"
-      if hash brew 2>/dev/null; then
-          log "Brew installed, ensuring up to date"
-          brew update
+    log "Boxen detected"
 
-          log "Installing lastpass-cli"
-          brew install lastpass-cli
-      else
-          log "I cant install lastpass-cli, so your secrets wont work at all"
+    if [[ $UPDATE == true ]]; then
+      log "update requested: Updating boxen"
+      boxen
+    fi
+  else
+    log "Non-boxened machine detected"
+    if hash brew 2>/dev/null; then
+      log "Brew installed"
+
+      if [[ $UPDATE == true ]]; then
+        log "update requested: Updating brew"
+        brew update
       fi
+
+      log "Installing lastpass-cli"
+      brew install lastpass-cli
+    else
+      log "I cant install lastpass-cli, so your secrets wont work at all"
+    fi
   fi
 }
