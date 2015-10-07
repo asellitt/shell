9971a12c1f94388c18704abaec91fe06c4015020
Anthony Sellitti <anthony.sellitti@envato.com>
2015-10-07 14:55:23 +1100

    use lastpass to manage secret keys

--- git/config.sh
+++ git/config.sh
@@ -6,9 +6,14 @@ log "Begin Git config"
 log "Installing Git config"
 GIT_DIR="${HOME}"
 GIT_MODULE_DIR="${DIR}/git"
+
 link "${GIT_DIR}/.gitconfig" "${GIT_MODULE_DIR}/config"
-git config --global core.excludesfile "${GIT_DIR}/.gitignore"
+lpass show --notes git.config.secret > "${DIR}/secret/git.config.secret"
+git config --global include.path "${DIR}/secret/git.config.secret"
+
 link "${GIT_DIR}/.gitignore" "${GIT_MODULE_DIR}/ignore"
+git config --global core.excludesfile "${GIT_DIR}/.gitignore"
+
 link "${GIT_DIR}/.git-completion.bash" "${GIT_MODULE_DIR}/completion.bash"
 link "${GIT_DIR}/.git-commands" "${GIT_MODULE_DIR}/commands"
 chmod -RH u+x "${GIT_DIR}/.git-commands"
