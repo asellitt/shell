f891cca32b2ed092dda2863b1bc33461d9fe3a7d
Anthony Sellitti <anthony.sellitti@envato.com>
2017-08-29 14:30:39 +1000

    Lets make sure that we have a ruby version manager. I like chruby, lets use that. While we're here. lets update to the latest ruby.
    My mac air is going to hate this. wheeeeee

--- ruby/config.sh
+++ ruby/config.sh
@@ -3,6 +3,15 @@
 PREFIX="RUBY"
 log "Begin Ruby config"
 
+log "Installing required ruby version manager"
+package 'chruby'
+package 'ruby-install'
+
+if hash ruby-install 2>/dev/null; then
+  log "Installing latest ruby"
+  ruby-install ruby --no-reinstall
+fi
+
 RUBY_DIR="${HOME}"
 RUBY_MODULE_DIR="${DOTFILES_DIR}/ruby"
 link "${RUBY_DIR}/.ruby-version" "${RUBY_MODULE_DIR}/version"
