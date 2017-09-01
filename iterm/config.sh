1208a9270ee55b4e6c5bf27ae2702328cccafca2
Anthony Sellitti <anthony.sellitti@envato.com>
2017-09-01 11:19:10 +1000

    lets get iterm under source control too

---
+++ iterm/config.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+PREFIX="ITERM"
+log "Begin iTerm config"
+
+if [ `uname` != 'Darwin' ]; then
+  log "This isn't OSX! Skipping config"
+else
+  log "OSX detected"
+  ITERM_MODULE_DIR="${DOTFILES_DIR}/iterm"
+  ITERM_PREFERENCES_DIR="${HOME}/Library/Preferences"
+
+  log "Installing preferences"
+  link "${ITERM_PREFERENCES_DIR}/com.googlecode.iterm2.plist" "${ITERM_MODULE_DIR}/com.googlecode.iterm2.plist"
+fi
