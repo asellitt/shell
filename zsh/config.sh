d7b4a7ce0129d127fb4111d3b9ea098115142c73
Anthony Sellitti <anthony.sellitti@envato.com>
2021-02-08 11:29:17 +1100

    update ohmyzsh when update is specified

--- zsh/config.sh
+++ zsh/config.sh
@@ -3,12 +3,23 @@
 PREFIX="ZSH"
 log "Begin Zsh config"
 
+ZSH_DIR="${HOME}"
+ZSH_MODULE_DIR="${DOTFILES_DIR}/zsh"
 local shell_version=$($SHELL --version)
 
 if [[ "${shell_version}" != *"zsh"* ]]; then
   log "Zsh not in use, skipping config"
 else
   log "Installing Zsh config"
-  log "Installing ohmyzsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  if [[ ! -d "${ZSH_DIR}/.oh-my-zsh" ]]; then
+    log "  Oh My Zsh not installed, installing"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  else
+    log "  Oh My Zsh installed"
+    if [[ $UPDATE == true ]]; then
+      log "  Updating Oh My Zsh"
+      exec ${ZSH_DIR}/.oh-my-zsh/tools/upgrade.sh
+    fi
+  fi
 fi
