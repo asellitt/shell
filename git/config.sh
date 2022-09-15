2ce533b304502a5c86b4a3f3e06d53e932d0e8fa
Anthony Sellitti <1116257+asellitt@users.noreply.github.com>
2022-09-15 11:40:32 +1000

    split git config up to reduce merge conflics cross boxes

--- git/config.sh
+++ git/config.sh
@@ -8,6 +8,13 @@ GIT_DIR="${HOME}"
 GIT_MODULE_DIR="${DOTFILES_DIR}/git"
 
 link "${GIT_DIR}/.gitconfig" "${GIT_MODULE_DIR}/config"
+link "${GIT_DIR}/.gitconfig.aliases" "${GIT_MODULE_DIR}/gitconfig/aliases"
+link "${GIT_DIR}/.gitconfig.commands" "${GIT_MODULE_DIR}/gitconfig/commands"
+link "${GIT_DIR}/.gitconfig.core" "${GIT_MODULE_DIR}/gitconfig/core"
+link "${GIT_DIR}/.gitconfig.diff" "${GIT_MODULE_DIR}/gitconfig/diff"
+link "${GIT_DIR}/.gitconfig.merge" "${GIT_MODULE_DIR}/gitconfig/merge"
+link "${GIT_DIR}/.gitconfig.user" "${GIT_MODULE_DIR}/gitconfig/user"
+
 if hash bw 2>/dev/null && hash jq 2>/dev/null; then
   log "Fetching git.config.secret"
   bw get item git.config.secret | \
