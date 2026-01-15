c1bc90430c625afad7fbb5fabb231d1f41bdca39
Anthony Sellitti <1116257+asellitt@users.noreply.github.com>
2026-01-16 08:25:22 +1100

    start centralising shell functionality

--- bash/config.sh
+++ bash/config.sh
@@ -5,6 +5,7 @@ log "Begin Bash config"
 
 BASH_DIR="${HOME}"
 BASH_MODULE_DIR="${DOTFILES_DIR}/bash"
+SHELL_MODULE_DIR="${DOTFILES_DIR}/shell"
 local shell_version=$($SHELL --version)
 
 if [[ "${shell_version}" != *"bash"* ]]; then
@@ -14,7 +15,7 @@ else
   link "${BASH_DIR}/.bashrc" "${BASH_MODULE_DIR}/rc"
   link "${BASH_DIR}/.bash_profile" "${BASH_MODULE_DIR}/profile"
   link "${BASH_DIR}/.bash_include" "${BASH_MODULE_DIR}/include"
-  link "${BASH_DIR}/.bash_aliases" "${BASH_MODULE_DIR}/aliases"
-  link "${BASH_DIR}/.bash_functions" "${BASH_MODULE_DIR}/functions"
   link "${BASH_DIR}/.bash_logout" "${BASH_MODULE_DIR}/logout"
+  link "${BASH_DIR}/.bash_aliases" "${SHELL_MODULE_DIR}/aliases"
+  link "${BASH_DIR}/.bash_functions" "${SHELL_MODULE_DIR}/functions"
 fi
